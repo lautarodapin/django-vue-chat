0e9de22097e76f72df7fe37e0613e5c05403e0d2
lautarodapin-magoya <lautaro@magoya.com>
2022-01-16 19:52:26 -0300

    Adds selected chat logic

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -3,9 +3,16 @@ import { ref } from "@vue/reactivity";
 import Login from "../src/components/Login.vue";
 import SideBar from "../src/components/SideBar.vue";
 import SideChat from "../src/components/SideChat.vue";
+import Chat from "../src/components/Chat.vue";
 import { useChats } from "../src/hooks/use-chats";
 const { chats, loading } = useChats();
 const selected = ref(4);
+const token = localStorage.getItem("token");
+
+const handleSelected = (id: number) => {
+  selected.value = id;
+  console.log(id);
+};
 </script>
 
 <template>
@@ -16,8 +23,12 @@ const selected = ref(4);
         :key="chat.id"
         :chat="chat"
         :selected="chat.id === selected"
+        @click="handleSelected"
       />
     </side-bar>
+    <div class="pl-64">
+      <chat :id="selected" />
+    </div>
     <!-- <div v-if="!token">
       <login />
     </div>
