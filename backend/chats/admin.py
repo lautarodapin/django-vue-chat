3e1d3fa08fa85f9ac4860f4d73e56285db585183
lautarodapin-magoya <lautaro@magoya.com>
2022-01-17 19:23:08 -0300

    Add admin inlien

--- backend/chats/admin.py
+++ backend/chats/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 from .models import Message, Chat
 
+
+class MessageInline(admin.TabularInline):
+    model = Message
+
+
 @admin.register(Chat)
 class ChatAdmin(admin.ModelAdmin):
-    pass
+    inlines = [MessageInline]
+
