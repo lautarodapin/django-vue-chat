2114b4821b0836c8b9f5b7b017d2310c7681cfa7
lautarodapin-magoya <lautaro@magoya.com>
2022-01-18 18:42:05 -0300

    Add svelte app

---
+++ ui-svelte/src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let count: number = 0;
+</script>
+
+<main>
+    <button on:click={() => count++}>+1</button>
+    {count}
+</main>
+
+<style lang="postcss" global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
