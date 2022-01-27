99016f8f8c53aed72cc72e11f6d3029549823367
lautarodapin-magoya <lautaro@magoya.com>
2022-01-27 19:33:22 -0300

    Borro basura

--- ui-svelte/src/stores/chat.ts
+++ ui-svelte/src/stores/chat.ts
@@ -2,5 +2,5 @@ import {writable} from 'svelte/store'
 import type {ChatDetail} from '../types'
 
 const search = new URLSearchParams(window.location.search)
-search.get
+
 export const chatSelected = writable<string>(search.get('chat') || null)
