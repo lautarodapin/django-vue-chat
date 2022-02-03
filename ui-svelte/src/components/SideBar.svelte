e766fd2ec4032e9cbece7b5c1f95346e54f38716
lautarodapin-magoya <lautaro@magoya.com>
2022-02-03 16:16:41 -0300

    logout para debug

--- ui-svelte/src/components/SideBar.svelte
+++ ui-svelte/src/components/SideBar.svelte
@@ -1,4 +1,11 @@
+<script lang="ts">
+    import { Token } from "../stores/token";
+</script>
+
 <nav class="fixed bg-slate-900 text-white top-0 left-0 h-screen w-64">
-    <h3 class="text-center my-8">Svelte chat</h3>
+    <div>
+        <h3 class="text-center my-8">Svelte chat</h3>
+        <button on:click={Token.signout}>Logout</button>
+    </div>
     <slot><!-- optional fallback --></slot>
 </nav>
