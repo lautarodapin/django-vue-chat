077c91d405e91b0ab52e5486ceaa795692ddb539
lautarodapin-magoya <lautaro@magoya.com>
2022-02-03 16:20:37 -0300

    No mostrar chats si no esta logeado

--- ui-svelte/src/components/SideBar.svelte
+++ ui-svelte/src/components/SideBar.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
     import { Token } from "../stores/token";
+
+    $: isAuth = !!$Token;
 </script>
 
 <nav class="fixed bg-slate-900 text-white top-0 left-0 h-screen w-64">
     <div>
         <h3 class="text-center my-8">Svelte chat</h3>
-        <button on:click={Token.signout}>Logout</button>
+        {#if isAuth}
+            <button on:click={Token.signout}>Logout</button>
+        {/if}
     </div>
-    <slot><!-- optional fallback --></slot>
+    {#if isAuth}
+        <slot><!-- optional fallback --></slot>
+    {/if}
 </nav>
