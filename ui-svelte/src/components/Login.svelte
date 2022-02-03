a7690f1c85992e382da42e36643814bb02966df2
lautarodapin <lautarodapin@gmail.com>
2022-02-03 20:18:06 -0300

    css y añadido errors

--- ui-svelte/src/components/Login.svelte
+++ ui-svelte/src/components/Login.svelte
@@ -1,38 +1,79 @@
 <script lang="ts">
-    import { Token } from "../stores/token";
+  import { fade } from "svelte/transition";
 
-    let username: string;
-    let password: string;
-    let loading: boolean;
+  import { Token } from "../stores/token";
+  import { formatDate } from "../utils/index";
 
-    const onSubmit = async () => {
-        loading = true;
-        const response = await fetch("http://localhost:8000/users/login/", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ username, password }),
-        });
-        const data = await response.json();
-        const { token } = data;
-        Token.signin(token);
-        loading = false;
-    };
+  let username: string;
+  let password: string;
+  let loading: boolean;
+  let errors: { username?: string; password?: string; formErrors?: string };
+  const onSubmit = async () => {
+    loading = true;
+    const response = await fetch("http://localhost:8000/users/login/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+    const data = await response.json();
+    console.log(response.ok);
+
+    if (response.ok) {
+      const { token } = data;
+      Token.signin(token);
+    } else {
+      errors = {
+        formErrors: data.non_field_errors?.join(", "),
+        password: data.password?.join(", "),
+        username: data.username?.join(", "),
+      };
+    }
+    loading = false;
+  };
+
+  $: console.log(errors);
 </script>
 
-<div class="h-screen flex items-center justify-center">
-    <form on:submit|preventDefault={onSubmit} class="">
-        <label for="username">Username</label>
-        <input id="username" bind:value={username} type="text" />
-        <label for="password">Password</label>
-        <input id="password" bind:value={password} type="password" />
-        <button type="submit">
-            {#if loading}
-                Login...
-            {:else}
-                Login
-            {/if}
-        </button>
-    </form>
+<div transition:fade class="h-screen flex items-center justify-center">
+  <form on:submit|preventDefault={onSubmit} class="grid grid-cols-1 gap-2">
+    <div>
+      <label for="username">Username</label>
+      <input
+        name="username"
+        bind:value={username}
+        type="text"
+        class="w-full min-w-[300px] h-10 focus:border-slate-700 rounded-lg border-slate-400 border-[.12rem] focus:border-[.2rem] px-2 py-2"
+      />
+      {#if errors?.username}
+        <div transition:fade class="text-red-600">{errors.username}</div>
+      {/if}
+    </div>
+    <div>
+      <label for="password">Password</label>
+      <input
+        name="password"
+        bind:value={password}
+        type="password"
+        class="w-full min-w-[300px] h-10 focus:border-slate-700 rounded-lg border-slate-400 border-[.12rem] focus:border-[.2rem] px-2 py-2"
+      />
+      {#if errors?.password}
+        <div transition:fade class="text-red-600">{errors.password}</div>
+      {/if}
+    </div>
+    {#if errors?.formErrors}
+      <div transition:fade class="text-red-600">{errors.formErrors}</div>
+    {/if}
+    <button
+      type="submit"
+      class="h-10 rounded-md bg-slate-700 text-white hover:bg-slate-400 hover:text-black"
+    >
+      {#if loading}
+        Login...
+      {:else}
+        Login
+      {/if}
+    </button>
+  </form>
 </div>
