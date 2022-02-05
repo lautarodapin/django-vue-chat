09f7275d3cb4ae5ba45b3b1ba9a19e09cbe7ee7a
lautarodapin-magoya <lautaro@magoya.com>
2022-02-04 22:35:24 -0300

    corregido mensaje

--- ui-svelte/src/components/messages/Message.svelte
+++ ui-svelte/src/components/messages/Message.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
-  import type { MessageDetail } from "../../types/index";
-  import { fromNow } from "../../utils/index";
-  export let { text, created_at, created_by: { username } = {} }: MessageDetail;
+    import { fade } from "svelte/transition";
+    import type { MessageDetail } from "../../types/index";
+    import { fromNow } from "../../utils/index";
+    export let message: MessageDetail;
+
+    // let { text, created_at, created_by: { username } = {} } = $message;
 </script>
 
 <div transition:fade>
-  {fromNow(created_at)} - {username}: {text}
+    <!-- {fromNow(created_at)} - {username}: {text} -->
+    {fromNow(message?.created_at)} - {message?.created_by.username}: {message.text}
 </div>
