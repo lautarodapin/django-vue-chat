65007c48022a303ff83560a4b99c3e6ddabba755
lautarodapin-magoya <lautaro@magoya.com>
2022-02-04 22:35:08 -0300

    boton logout se movio al final del sidebar

--- ui-svelte/src/components/SideBar.svelte
+++ ui-svelte/src/components/SideBar.svelte
@@ -1,17 +1,25 @@
 <script lang="ts">
     import { Token } from "../stores/token";
-
+    import { fade } from "svelte/transition";
     $: isAuth = !!$Token;
 </script>
 
 <nav class="fixed bg-slate-900 text-white top-0 left-0 h-screen w-64">
     <div>
         <h3 class="text-center my-8">Svelte chat</h3>
-        {#if isAuth}
-            <button on:click={Token.signout}>Logout</button>
-        {/if}
     </div>
     {#if isAuth}
         <slot><!-- optional fallback --></slot>
+    {/if}
+    {#if isAuth}
+        <div
+            transition:fade
+            class="absolute bottom-4 flex w-60 h-12 justify-center"
+        >
+            <button
+                class="rounded-md bg-slate-600 border-slate-600 text-white hover:bg-slate-400 hover:text-black w-40 hover:border-slate-400"
+                on:click={Token.signout}>Logout</button
+            >
+        </div>
     {/if}
 </nav>
