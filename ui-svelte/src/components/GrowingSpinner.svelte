a34bb7e96ba16bff4186a346a4a548e12bc43bc7
lautarodapin-magoya <lautaro@magoya.com>
2022-02-04 22:34:45 -0300

    loading spinner

---
+++ ui-svelte/src/components/GrowingSpinner.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let size: "8" | "16" | "32" | "64" = "32";
+</script>
+
+<div class="flex justify-center items-center">
+    <div
+        class={`animate-spin rounded-full h-${size} w-${size} border-b-2 border-slate-200`}
+    />
+</div>
