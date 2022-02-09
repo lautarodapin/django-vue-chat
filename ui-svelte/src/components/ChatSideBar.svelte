eebbabd8906aecc9da2fa2f16b46c9976fd33588
lautarodapin <lautarodapin@gmail.com>
2022-02-09 20:13:45 -0300

    añadido emit para ordenar

--- ui-svelte/src/components/ChatSideBar.svelte
+++ ui-svelte/src/components/ChatSideBar.svelte
@@ -1,49 +1,51 @@
 <script lang="ts">
-    import { fade } from "svelte/transition";
-    import { useWebsocket } from "../composables/use-websocket";
-    import { chatSelected } from "../stores/chat";
-    import { websocket } from "../stores/websocket";
-    import { Actions, ChatDetail, MessageDetail } from "../types";
+  import { createEventDispatcher } from "svelte";
+  import { fade } from "svelte/transition";
+  import { chatSelected } from "../stores/chat";
+  import { websocket } from "../stores/websocket";
+  import { Actions, ChatDetail, MessageDetail } from "../types";
 
-    export let chat: ChatDetail;
+  export let chat: ChatDetail;
 
-    const handleClick = (chat: ChatDetail) => {
-        const searchParams = new URLSearchParams(window.location.search);
-        searchParams.set("chat", chat.id.toString());
-        history.pushState(
-            null,
-            "",
-            window.location.pathname + "?" + searchParams.toString()
-        );
-        chatSelected.update(() => chat.id.toString());
-    };
-    $: {
-        $websocket?.addEventListener("open", () => {
-            console.log("chat side bar open", chat.id);
-            $websocket?.send(
-                JSON.stringify({
-                    action: Actions.SubscribeToChat,
-                    id: chat.id,
-                    request_id: Math.random(),
-                })
-            );
-        });
-        $websocket?.addEventListener("message", (e) => {
-            const data: MessageDetail = JSON.parse(e.data);
-            if (data.chat !== chat.id) return;
-            chat.last_message = data;
-        });
-    }
+  const dispatch = createEventDispatcher<{ newMessage: ChatDetail }>();
+  const handleClick = (chat: ChatDetail) => {
+    const searchParams = new URLSearchParams(window.location.search);
+    searchParams.set("chat", chat.id.toString());
+    history.pushState(
+      null,
+      "",
+      window.location.pathname + "?" + searchParams.toString()
+    );
+    chatSelected.update(() => chat.id.toString());
+  };
+  $: {
+    $websocket?.addEventListener("open", () => {
+      console.log("chat side bar open", chat.id);
+      $websocket?.send(
+        JSON.stringify({
+          action: Actions.SubscribeToChat,
+          id: chat.id,
+          request_id: Math.random(),
+        })
+      );
+    });
+    $websocket?.addEventListener("message", (e) => {
+      const data: MessageDetail = JSON.parse(e.data);
+      if (data.chat !== chat.id) return;
+      chat.last_message = data;
+      dispatch("newMessage", chat);
+    });
+  }
 </script>
 
 <div
-    transition:fade
-    on:click={() => handleClick(chat)}
-    class="bg-slate-800 py-8 px-2 hover:bg-slate-300 hover:cursor-pointer"
-    class:bg-slate-500={Number($chatSelected) === chat.id}
+  transition:fade
+  on:click={() => handleClick(chat)}
+  class="bg-slate-800 py-8 px-2 hover:bg-slate-300 hover:cursor-pointer"
+  class:bg-slate-500={Number($chatSelected) === chat.id}
 >
-    {chat.id}
-    {#if chat.last_message && chat.last_message.created_by}
-        {chat.last_message?.created_by?.username}: {chat.last_message?.text}
-    {/if}
+  {chat.id}
+  {#if chat.last_message && chat.last_message.created_by}
+    {chat.last_message?.created_by?.username}: {chat.last_message?.text}
+  {/if}
 </div>
--- ui-svelte/src/components/Chats.svelte
+++ ui-svelte/src/components/Chats.svelte
@@ -1,47 +1,72 @@
 <script lang="ts">
-    import { fade } from "svelte/transition";
-    import { useWebsocket } from "../composables/use-websocket";
-    import { chatSelected as chatStore } from "../stores/chat";
-    import type { ChatDetail, ChatList } from "../types";
-    import ChatSideBar from "./ChatSideBar.svelte";
-    import GrowingSpinner from "./GrowingSpinner.svelte";
+  import dayjs from "dayjs";
+  import { onMount } from "svelte";
+  import { fade } from "svelte/transition";
+  import { chatSelected as chatStore } from "../stores/chat";
+  import type { ChatDetail, ChatList } from "../types";
+  import ChatSideBar from "./ChatSideBar.svelte";
+  import GrowingSpinner from "./GrowingSpinner.svelte";
+  let chats: ChatList["results"] = [];
+  let loading = false;
 
-    const getChats = async () => {
-        const response = await fetch("http://localhost:8000/chats/", {
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Token ${localStorage.getItem("token")}`,
-            },
-        });
-        const data: ChatList = await response.json();
-        chatStore.update((old) => {
-            if (old) return old;
-            return data.results[0].id.toString();
-        });
-        return data.results;
-    };
+  onMount(() => {
+    getChats();
+  });
 
-    const handleClick = (chat: ChatDetail) => {
-        const searchParams = new URLSearchParams(window.location.search);
-        searchParams.set("chat", chat.id.toString());
-        history.pushState(
-            null,
-            "",
-            window.location.pathname + "?" + searchParams.toString()
-        );
-        chatStore.update(() => chat.id.toString());
-    };
+  const getChats = async () => {
+    loading = true;
+    const response = await fetch("http://localhost:8000/chats/", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${localStorage.getItem("token")}`,
+      },
+    });
+    const data: ChatList = await response.json();
+    chatStore.update((old) => {
+      if (old) return old;
+      return data.results[0].id.toString();
+    });
+    if (data.results)
+      chats = data.results.sort((a, b) =>
+        dayjs(a.last_message.created_at).isBefore(
+          dayjs(b.last_message.created_at)
+        )
+          ? 1
+          : -1
+      );
+  };
 
-    $: selectedChat = $chatStore;
-    let chatsPromise = getChats();
+  const handleClick = (chat: ChatDetail) => {
+    const searchParams = new URLSearchParams(window.location.search);
+    searchParams.set("chat", chat.id.toString());
+    history.pushState(
+      null,
+      "",
+      window.location.pathname + "?" + searchParams.toString()
+    );
+    chatStore.update(() => chat.id.toString());
+  };
 </script>
 
-{#await chatsPromise}
-    <div transition:fade>
-        <GrowingSpinner size="16" />
-    </div>
-{:then chats}
-    {#each chats as chat}
-        <ChatSideBar {chat} />
-    {/each}
-{/await}
+{#if !loading}
+  <div transition:fade>
+    <GrowingSpinner size="16" />
+  </div>
+{:else}
+  {#each chats as chat}
+    <ChatSideBar
+      {chat}
+      on:newMessage={({ detail: chat }) => {
+        chats = chats
+          .map((c) => (c.id === chat.id ? { ...chat } : c))
+          .sort((a, b) =>
+            dayjs(a.last_message.created_at).isBefore(
+              dayjs(b.last_message.created_at)
+            )
+              ? 1
+              : -1
+          );
+      }}
+    />
+  {/each}
+{/if}
