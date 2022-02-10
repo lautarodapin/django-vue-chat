2a5f37b27dfc825bd5d18cda713b483bab464b79
lautarodapin-magoya <lautaro@magoya.com>
2022-02-10 19:28:39 -0300

    mensajes no leidos en un badge

--- ui-svelte/src/components/ChatSideBar.svelte
+++ ui-svelte/src/components/ChatSideBar.svelte
@@ -1,51 +1,68 @@
 <script lang="ts">
-  import { createEventDispatcher } from "svelte";
-  import { fade } from "svelte/transition";
-  import { chatSelected } from "../stores/chat";
-  import { websocket } from "../stores/websocket";
-  import { Actions, ChatDetail, MessageDetail } from "../types";
+    import { createEventDispatcher } from "svelte";
+    import { fade } from "svelte/transition";
+    import { chatSelected } from "../stores/chat";
+    import { websocket } from "../stores/websocket";
+    import { Actions, ChatDetail, MessageDetail } from "../types";
 
-  export let chat: ChatDetail;
+    export let chat: ChatDetail;
+    $: ({
+        id,
+        unread_count,
+        last_message: {
+            text,
+            created_by: { username },
+        },
+    } = chat);
 
-  const dispatch = createEventDispatcher<{ newMessage: ChatDetail }>();
-  const handleClick = (chat: ChatDetail) => {
-    const searchParams = new URLSearchParams(window.location.search);
-    searchParams.set("chat", chat.id.toString());
-    history.pushState(
-      null,
-      "",
-      window.location.pathname + "?" + searchParams.toString()
-    );
-    chatSelected.update(() => chat.id.toString());
-  };
-  $: {
-    $websocket?.addEventListener("open", () => {
-      console.log("chat side bar open", chat.id);
-      $websocket?.send(
-        JSON.stringify({
-          action: Actions.SubscribeToChat,
-          id: chat.id,
-          request_id: Math.random(),
-        })
-      );
-    });
-    $websocket?.addEventListener("message", (e) => {
-      const data: MessageDetail = JSON.parse(e.data);
-      if (data.chat !== chat.id) return;
-      chat.last_message = data;
-      dispatch("newMessage", chat);
-    });
-  }
+    const dispatch = createEventDispatcher<{ newMessage: ChatDetail }>();
+    const handleClick = (chat: ChatDetail) => {
+        const searchParams = new URLSearchParams(window.location.search);
+        searchParams.set("chat", chat.id.toString());
+        history.pushState(
+            null,
+            "",
+            window.location.pathname + "?" + searchParams.toString()
+        );
+        chatSelected.update(() => chat.id.toString());
+    };
+    $: {
+        $websocket?.addEventListener("open", () => {
+            console.log("chat side bar open", chat.id);
+            $websocket?.send(
+                JSON.stringify({
+                    action: Actions.SubscribeToChat,
+                    id: chat.id,
+                    request_id: Math.random(),
+                })
+            );
+        });
+        $websocket?.addEventListener("message", (e) => {
+            const data: MessageDetail = JSON.parse(e.data);
+            if (data.chat !== chat.id) return;
+            chat.last_message = data;
+            dispatch("newMessage", chat);
+        });
+    }
 </script>
 
 <div
-  transition:fade
-  on:click={() => handleClick(chat)}
-  class="bg-slate-800 py-8 px-2 hover:bg-slate-300 hover:cursor-pointer"
-  class:bg-slate-500={Number($chatSelected) === chat.id}
+    transition:fade
+    on:click={() => handleClick(chat)}
+    class="bg-slate-800 py-8 px-2 hover:bg-slate-300 hover:cursor-pointer"
+    class:bg-slate-500={Number($chatSelected) === id}
 >
-  {chat.id}
-  {#if chat.last_message && chat.last_message.created_by}
-    {chat.last_message?.created_by?.username}: {chat.last_message?.text}
-  {/if}
+    <span class="">
+        ({id})
+        {#if username}
+            {username}: {text}
+        {/if}
+    </span>
+    {#if unread_count && unread_count > 0}
+        <span
+            class="inline-flex items-center p-1 mr-2 text-[0.625rem] font-semibold text-white bg-red-500 rounded-full"
+        >
+            {unread_count}
+        </span>
+    {/if}
 </div>
