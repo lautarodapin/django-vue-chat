f7d0a74a0538ddaabbbf9667c6a2142db6df0359
lautarodapin-magoya <lautaro@magoya.com>
2022-02-10 19:27:36 -0300

    anadido read boolean

--- backend/chats/models.py
+++ backend/chats/models.py
@@ -15,6 +15,7 @@ class Chat(TimestampedModel):
 class Message(TimestampedModel):
     text = models.CharField(max_length=1024)
     chat = models.ForeignKey(Chat, models.SET_NULL, null=True, blank=False, related_name='messages')
+    read = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.created_by}: {self.text}' if self.created_by else 'Usuario borrado'
